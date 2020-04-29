b135df6b9eaf327591ab066c3bc6733c69a1e9a2
Jakub Raban <jakub.raban@gmail.com>
2020-04-29 10:59:25 +0200

    Load a simple template

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Dzień dobry z tej strony Małgorzata Żabińska-Rakoczy"
+    all_our_faults = [
+        'U was jest pięć osób i trzeba żebyście mieli dużo',
+        'To już by się przydało jakieś analizy przygototać',
+        'Państwo chyba się skupiacie na innych projektach tudzież pracy zawodowej',
+        'Te cele to o kant dupy rozbić',
+        'Ten diagram Gantta to chyba tak na odczep się, nie zachodzi na siebie',
+        'Łerkfloł jest zły',
+        'Żródła należy sortować alfabetycznie'
+    ]
+    return render_template('index.html', faults=all_our_faults)

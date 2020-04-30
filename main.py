455b536c7ea0024001b1a77ce3de7b55f98e95f8
Jakub Raban <jakub.raban@gmail.com>
2020-04-30 07:57:49 +0200

    Added preloading database

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017')
 db = client.get_database('tweets')
+collection = db['test1']
 
 
 @app.route('/')

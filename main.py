730a8f5a6b22bde207247caf7c1619503877b8d4
Jakub Raban <jakub.raban@gmail.com>
2020-04-30 23:12:16 +0200

    Use g-object to store database connection | change env-vars | move password to docker secrets

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://root:rootpassword@db:27017')
-db = client.get_database('tweets')
-collection = db['test1']
+
+
+def get_db():
+    if 'db' not in g:
+        client = MongoClient('mongodb://root:rootpassword@db:27017')
+        g.db = client.get_database('tweets')
+    return g.db
+
+
+def get_test_collection():
+    if 'test_collection' not in g:
+        g.test_collection = get_db()['test1']
+    return g.test_collection
 
 
 @app.route('/')
 def hello():
-    cursor = collection.find({})
+    cursor = get_test_collection().find({})
     all_our_faults = [doc['fault'] for doc in cursor]
     return render_template('index.html', faults=all_our_faults)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')

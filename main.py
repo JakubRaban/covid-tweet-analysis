c1112b8b4f6f8baaeec7266a7f6264f91095d523
Jakub Raban <jakub.raban@gmail.com>
2020-04-30 09:33:03 +0200

    Changed initializer to docker-entrypoint-initdb-based

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
+
 from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017')
+client = MongoClient('mongodb://root:rootpassword@db:27017')
 db = client.get_database('tweets')
 collection = db['test1']
 
 
 @app.route('/')
 def hello():
-    all_our_faults = [
-        'U was jest pięć osób i trzeba żebyście mieli dużo',
-        'To już by się przydało jakieś analizy przygototać',
-        'Państwo chyba się skupiacie na innych projektach tudzież pracy zawodowej',
-        'Te cele to o kant dupy rozbić',
-        'Ten diagram Gantta to chyba tak na odczep się, nie zachodzi na siebie',
-        'Łerkfloł jest zły',
-        'Żródła należy sortować alfabetycznie'
-    ]
+    cursor = collection.find({})
+    all_our_faults = [doc['fault'] for doc in cursor]
     return render_template('index.html', faults=all_our_faults)
 
 

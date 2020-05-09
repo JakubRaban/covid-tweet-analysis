23a93e087196c839f262cde31aa6c846f7c445b3
FizzyJelly <jakp1101@gmail.com>
2020-05-09 13:50:36 +0200

    Support for standard API queries

--- fetch/main.py
+++ fetch/main.py
@@ -9,9 +9,9 @@ from fetch import twitter, database
 
 TWITTER_API_KEY_ENVVAR = "TWITTER_API_KEY"
 TWITTER_API_SECRET_ENVVAR = "TWITTER_API_SECRET"
-TWITTER_API_APP_NAME_ENVVAR = "TWITTER_API_APP_NAME"
+TWITTER_API_DEV_ENV_ENVVAR = "TWITTER_API_DEV_ENV"
 MONGODB_CONNECTION_URL_ENVVAR = "MONGODB_CONNECTION_URL"
-MONGODB_DBNAME_ENVVAR = "MONGDB_DBNAME"
+MONGODB_DBNAME_ENVVAR = "MONGODB_DBNAME"
 
 
 async def fetch(query, source, database, collection):
@@ -19,23 +19,29 @@ async def fetch(query, source, database, collection):
         await database.insert(tweet, collection)
 
 
-async def main(query, twitter_api, collection):
-    twitter_query = twitter.Query(query_string=query, to_date=datetime(2020, 4, 20), from_date=datetime(2020, 4, 16), max_results=10000)
-
+async def main(query, twitter_api, collection, product, max_tweets):
     if twitter_api:
         credentials = twitter.Credentials(
             api_key=os.getenv(TWITTER_API_KEY_ENVVAR),
             secret_key=os.getenv(TWITTER_API_SECRET_ENVVAR),
-            app_name=os.getenv(TWITTER_API_APP_NAME_ENVVAR),
+            dev_env=os.getenv(TWITTER_API_DEV_ENV_ENVVAR),
         )
         if not all(credentials):
             print(
-                f"You must specify {TWITTER_API_KEY_ENVVAR}, {TWITTER_API_APP_NAME_ENVVAR} "
+                f"You must specify {TWITTER_API_KEY_ENVVAR}, {TWITTER_API_DEV_ENV_ENVVAR} "
                 f"and {TWITTER_API_SECRET_ENVVAR} environment variables"
             )
             return
 
-        source = await twitter.Client.from_api_key(credentials)
+        if product == '7day':
+            source = await twitter.StandardClient.from_api_key(credentials)
+            twitter_query = twitter.StandardQuery(query_string=query, max_results=max_tweets)
+        else:
+            source = await twitter.PremiumClient.from_api_key(credentials)
+            source.product = product
+            twitter_query = twitter.PremiumQuery(query_string=query, to_date=datetime(2020, 4, 20),
+                                                 from_date=datetime(2020, 4, 16), max_results=max_tweets)
+
     else:
         source = twitter.FakeClient()
 
@@ -62,8 +68,21 @@ async def main(query, twitter_api, collection):
     default="Influencerzy",
     help="Mongo collection to insert tweets to",
 )
-def fetch_command(query, twitter_api, mongo_collection):
-    asyncio.run(main(query, twitter_api, mongo_collection))
+@click.option(
+    '--endpoint-product',
+    '-p',
+    default='7day',
+    type=click.Choice(['7day', '30day', 'fullarchive']),
+    help='Twitter API endpoint product'
+)
+@click.option(
+    '--max-tweets',
+    '-m',
+    default=10000,
+    help='Maximum number of tweets to fetch'
+)
+def fetch_command(query, twitter_api, mongo_collection, endpoint_product, max_tweets):
+    asyncio.run(main(query, twitter_api, mongo_collection, endpoint_product, max_tweets))
 
 
 if __name__ == "__main__":
--- fetch/twitter.py
+++ fetch/twitter.py
@@ -1,19 +1,17 @@
 import abc
-from collections import namedtuple, AsyncIterator
+import datetime
+from collections import AsyncIterator, namedtuple
 from contextlib import asynccontextmanager
 from typing import Dict
 
 import aiohttp
-import datetime
 
 
 async def retrieve_bearer_token(api_key, api_secret_key) -> str:
     auth = aiohttp.BasicAuth(api_key, api_secret_key)
     async with aiohttp.ClientSession() as session:
         async with session.post(
-            "https://api.twitter.com/oauth2/token",
-            data={"grant_type": "client_credentials"},
-            auth=auth,
+            "https://api.twitter.com/oauth2/token", data={"grant_type": "client_credentials"}, auth=auth,
         ) as r:
             if r.status == 403:
                 raise Exception("Invaild client credentials")
@@ -24,7 +22,20 @@ async def retrieve_bearer_token(api_key, api_secret_key) -> str:
             return json_response["access_token"]
 
 
-class Query:
+TWEETS_PER_REQUEST = 100
+
+class APIQuery(abc.ABC):
+
+    @abc.abstractmethod
+    def to_query_dict(self) -> Dict:
+        ...
+
+    @abc.abstractstaticmethod
+    def _datetime_to_twitter_format(datetime: datetime.datetime) -> str:
+        ...
+
+
+class PremiumQuery(APIQuery):
     def __init__(
         self,
         query_string: str,
@@ -55,47 +66,73 @@ class Query:
         return self._max_results
 
 
-TWEETS_PER_REQUEST = 100
+class StandardQuery(APIQuery):
+    def __init__(
+        self,
+        query_string: str,
+        max_results: int = 250,
+        lang: str or None = None,
+        until: datetime.datetime or None = None,
+        tweet_mode: str = 'extended'
+    ):
+        self._query_string = query_string
+        self._max_results = max_results
+        self._lang = lang
+        self._until = until or datetime.datetime.now()
+        self._tweet_mode = tweet_mode
+
+    @staticmethod
+    def _datetime_to_twitter_format(datetime: datetime.datetime) -> str:
+        return datetime.strftime("%Y-%m-%d")
+
+    def to_query_dict(self) -> Dict:
+        return {
+            "q": self._query_string,
+            "count": TWEETS_PER_REQUEST,
+            "until": self._datetime_to_twitter_format(self._until),
+            'tweet_mode': self._tweet_mode,
+            'lang': self._lang or ''
+        }
+
+    @property
+    def results(self) -> int:
+        return self._max_results
 
 
-Credentials = namedtuple("Credentials", ("api_key", "secret_key", "app_name"))
+Credentials = namedtuple("Credentials", ("api_key", "secret_key", "dev_env"))
 
 
-class TweetSource(abc.ABC):
-    @abc.abstractmethod
-    async def count(self, query):
-        pass
-
-    @abc.abstractmethod
-    async def query(self, query):
-        pass
-
-
-class Client(TweetSource):
-    def __init__(self, bearer_token: str, app_name: str):
+class Client(abc.ABC):
+    def __init__(self, bearer_token: str, dev_env: str):
         self._bearer_token = bearer_token
-        self.app_name = app_name
+        self.dev_env = dev_env
 
     @classmethod
     async def from_api_key(cls, credentials: Credentials) -> "Client":
-        bearer_token = await retrieve_bearer_token(
-            credentials.api_key, credentials.secret_key
-        )
-        return cls(bearer_token, credentials.app_name)
+        bearer_token = await retrieve_bearer_token(credentials.api_key, credentials.secret_key)
+        return cls(bearer_token, credentials.dev_env)
 
     @property
     def _auth_headers(self):
         return {"Authorization": f"Bearer {self._bearer_token}"}
 
+    @abc.abstractmethod
+    async def query(self, query: APIQuery):
+        pass
+
+
+class PremiumClient(Client):
+    def __init__(self, *args, product, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.product = product
+
     @asynccontextmanager
     async def _twitter_api_request(self, url, **kwargs):
         async with aiohttp.ClientSession() as session:
-            async with session.post(
-                url, headers=self._auth_headers, **kwargs
-            ) as result:
+            async with session.post(url, headers=self._auth_headers, **kwargs) as result:
                 yield result
 
-    async def query(self, query: Query) -> AsyncIterator:
+    async def query(self, query: PremiumQuery) -> AsyncIterator:
         query_dict = query.to_query_dict()
         query_dict["maxResults"] = TWEETS_PER_REQUEST
 
@@ -103,12 +140,12 @@ class Client(TweetSource):
 
         while remaining_results > 0:
             async with self._twitter_api_request(
-                f"https://api.twitter.com/1.1/tweets/search/30day/{self.app_name}.json",
-                json=query_dict,
+                f"https://api.twitter.com/1.1/tweets/search/{self.product}/{self.dev_env}.json",
+                json=query_dict
             ) as r:
                 json_response = await r.json()
                 if r.status != 200:
-                    raise Exception("Tweet retrieval error")
+                    raise Exception(f"Tweet retrieval error, status: {r.status}, {await r.text()}")
 
                 for tweet in json_response["results"]:
                     yield tweet
@@ -119,13 +156,12 @@ class Client(TweetSource):
                 else:
                     break
 
-    async def count(self, query: Query) -> int:
+    async def count(self, query: PremiumQuery) -> int:
         query_dict = query.to_query_dict()
         del query_dict["maxResults"]
 
         async with self._twitter_api_request(
-            f"https://api.twitter.com/1.1/tweets/search/30day/{self.app_name}/counts.json",
-            json=query_dict,
+            f"https://api.twitter.com/1.1/tweets/search/30day/{self.app_name}/counts.json", json=query_dict,
         ) as r:
             json_response = await r.json()
             if r.status != 200:
@@ -134,11 +170,45 @@ class Client(TweetSource):
             return json_response["totalCount"]
 
 
-class FakeClient(TweetSource):
-    async def count(self, query: Query) -> int:
+class StandardClient(Client):
+    @asynccontextmanager
+    async def _twitter_api_request(self, url, **kwargs):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=self._auth_headers, **kwargs) as result:
+                yield result
+
+    async def query(self, query: StandardQuery) -> AsyncIterator:
+        query_dict = query.to_query_dict()
+
+        remaining_results = query.results
+
+        while remaining_results > 0:
+            async with self._twitter_api_request(
+                f"https://api.twitter.com/1.1/search/tweets.json",
+                params=query_dict
+            ) as r:
+                json_response = await r.json()
+                if r.status != 200:
+                    raise Exception(f"Tweet retrieval error, status: {r.status}, {await r.text()}")
+
+                for tweet in json_response["statuses"]:
+                    yield tweet
+
+                remaining_results -= TWEETS_PER_REQUEST
+                if "next" in json_response:
+                    query_dict["next"] = json_response["next"]
+                else:
+                    break
+
+
+class FakeClient(PremiumClient):
+    def __init__(self) -> None:
+        pass
+
+    async def count(self, query: APIQuery) -> int:
         return 1
 
-    async def query(self, query: Query) -> AsyncIterator:
+    async def query(self, query: APIQuery) -> AsyncIterator:
         yield {"__test": True, "id": 2137}
         yield {"__test": True, "id": 1488}
         yield {"__test": True, "id": 420}

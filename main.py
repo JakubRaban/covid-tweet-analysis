0021d3160d18d22da40eb84a68eff3063c029082
Jakub Raban <jakub.raban@gmail.com>
2020-05-16 17:35:25 +0200

    Datatable for homepage

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, g
 from pymongo import MongoClient
 
 from data_source import TweetSource
+import requests
 
 app = Flask(__name__)
 
@@ -35,6 +36,12 @@ def get_tweet_source(g):
     return TweetSource(get_db())
 
 
+def get_embeddable_tweet_html_by_id(tweet_id):
+    r = requests.get('https://publish.twitter.com/oembed', params={'url': f'https://twitter.com/i/statuses/{tweet_id}',
+                                                                   'maxwidth': 550})
+    return r.json()['html']
+
+
 @app.route('/')
 def hello():
     tweet_source = get_tweet_source()
@@ -42,7 +49,41 @@ def hello():
         group: [repr(tweet_source.get_tweets((group,)).to_data_frame()['text'])]
         for group in tweet_source.collection_names
     }
-    return render_template('index.html', homepage=homepage)
+    users = [
+        {
+            'username': 'Andrzej Duda',
+            'social_group': 'Politycy',
+            'covid_tweet_count': 12,
+            'average_favourites': 5000,
+            'most_favourites': 17000,
+            'average_retweets': 2000
+        },
+        {
+            'username': 'Robert Biedroń',
+            'social_group': 'Politycy',
+            'covid_tweet_count': 53,
+            'average_favourites': 800,
+            'most_favourites': 3000,
+            'average_retweets': 400
+        },
+        {
+            'username': 'Dorota Gawryluk',
+            'social_group': 'Dziennikarze',
+            'covid_tweet_count': 102,
+            'average_favourites': 590,
+            'most_favourites': 1700,
+            'average_retweets': 120
+        }
+    ]
+    return render_template('homepage.html', homepage=homepage, users=users)
+
+
+@app.route('/tweet-test')
+def tweet_test():
+    t = get_db()['Lekarze'].find({})[0]['id_str']
+    html = get_embeddable_tweet_html_by_id(t)
+    print(html)
+    return render_template('tweet.html', xdd=html)
 
 
 if __name__ == '__main__':

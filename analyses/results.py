6abf9c789a41df767c0d7d2b787855dd9ceb0d3a
Mirosław <miro662@gmail.com>
2020-05-20 11:52:44 +0200

    Analysis results

---
+++ analyses/results.py
@@ -0,0 +1,79 @@
+import abc
+import base64
+from io import BytesIO
+from typing import Dict
+
+import flask
+import pandas as pd
+from matplotlib.figure import Figure
+
+
+class AnalysisResult(abc.ABC):
+    """ Encapsulates result of analysis in a way that is easy to render
+    """
+
+    @abc.abstractmethodblac
+    def render_html(self) -> str:
+        """ Renders result to an HTML
+
+        :return: Ready to-be-rendered HTML containing analysis result. It is a safe string
+        """
+
+
+class TextAnalysisResult(AnalysisResult):
+    def __init__(self, text: str):
+        """
+        :param text: Text to be displayed as analysis result
+        """
+        self._text = text
+
+    def render_html(self) -> str:
+        return flask.escape(self._text)
+
+
+class CompositeAnalysisResult(AnalysisResult):
+    """ Composes and displays different analyses results as one result
+    """
+
+    def __init__(self, **kwargs: Dict[str, AnalysisResult]):
+        """
+        :param kwargs: Analyses to be composed, identified by their names
+        """
+        self._analyses = kwargs
+
+    def render_html(self) -> str:
+        return "\n".join(
+            self._render_analysis_item(title, result)
+            for title, result in self._analyses.items()
+        )
+
+    @staticmethod
+    def _render_analysis_item(title: str, result: AnalysisResult):
+        escaped_title = flask.escape(title)
+        rendered_result = result.render_html()
+        return f"<h2>{escaped_title}</h2><section>{rendered_result}</section>"
+
+
+class DataFrameAnalysisResult(AnalysisResult):
+    def __init__(self, dataframe: pd.DataFrame):
+        """
+        :param text: DataFrame to be displayed as analysis result
+        """
+        self._dataframe = dataframe
+
+    def render_html(self) -> str:
+        return self._dataframe.to_html()
+
+
+class FigureAnalysisResult(AnalysisResult):
+    def __init__(self, figure: Figure):
+        """
+        :param text: Matplotlib's figure to be displayed as analysis result
+        """
+        self._figure = figure
+
+    def render_html(self) -> str:
+        buffer = BytesIO()
+        self._figure.savefig(buffer, format="png")
+        data = base64.b64encode(buffer.getbuffer()).decode("ascii")
+        return f"<img src='data:image/png;base64,{data}'> /"
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-
 from flask import Flask, render_template, g
+import pandas as pd
+from matplotlib.figure import Figure
 from pymongo import MongoClient
 
+from analyses import results
 from data_source import TweetSource
 import requests
 
@@ -16,19 +18,21 @@ def g_factory(g):
             if name not in g:
                 setattr(g, name, func(g))
             return getattr(g, name)
+
         return result_func
+
     return decorator
 
 
 @g_factory(g)
 def get_db(g):
-    client = MongoClient(f'mongodb://root:rootpassword@db:27017')
-    return client.get_database('tweets')
+    client = MongoClient(f"mongodb://root:rootpassword@db:27017")
+    return client.get_database("tweets")
 
 
 @g_factory(g)
 def get_test_collection(g):
-    return get_db()['test1']
+    return get_db()["test1"]
 
 
 @g_factory(g)
@@ -37,105 +41,132 @@ def get_tweet_source(g):
 
 
 def get_embeddable_tweet_html_by_id(tweet_id):
-    r = requests.get('https://publish.twitter.com/oembed', params={'url': f'https://twitter.com/i/statuses/{tweet_id}',
-                                                                   'maxwidth': 550})
-    return r.json()['html']
+    r = requests.get(
+        "https://publish.twitter.com/oembed",
+        params={"url": f"https://twitter.com/i/statuses/{tweet_id}", "maxwidth": 550},
+    )
+    return r.json()["html"]
 
 
-@app.route('/')
+@app.route("/")
 def homepage_view():
     tweet_source = get_tweet_source()
     homepage = {
-        group: [repr(tweet_source.get_tweets((group,)).to_data_frame()['text'])]
+        group: [repr(tweet_source.get_tweets((group,)).to_data_frame()["text"])]
         for group in tweet_source.collection_names
     }
     users = [
         {
-            'username': 'Andrzej Duda',
-            'social_group': 'Politycy',
-            'covid_tweet_count': 12,
-            'average_favourites': 5000,
-            'most_favourites': 17000,
-            'average_retweets': 2000
+            "username": "Andrzej Duda",
+            "social_group": "Politycy",
+            "covid_tweet_count": 12,
+            "average_favourites": 5000,
+            "most_favourites": 17000,
+            "average_retweets": 2000,
         },
         {
-            'username': 'Robert Biedroń',
-            'social_group': 'Politycy',
-            'covid_tweet_count': 53,
-            'average_favourites': 800,
-            'most_favourites': 3000,
-            'average_retweets': 400
+            "username": "Robert Biedroń",
+            "social_group": "Politycy",
+            "covid_tweet_count": 53,
+            "average_favourites": 800,
+            "most_favourites": 3000,
+            "average_retweets": 400,
         },
         {
-            'username': 'Dorota Gawryluk',
-            'social_group': 'Dziennikarze',
-            'covid_tweet_count': 102,
-            'average_favourites': 590,
-            'most_favourites': 1700,
-            'average_retweets': 120
-        }
+            "username": "Dorota Gawryluk",
+            "social_group": "Dziennikarze",
+            "covid_tweet_count": 102,
+            "average_favourites": 590,
+            "most_favourites": 1700,
+            "average_retweets": 120,
+        },
     ]
-    return render_template('homepage.html', users=users)
+    return render_template("homepage.html", users=users)
 
 
-@app.route('/user-summary/<username>')
+@app.route("/user-summary/<username>")
 def user_summary(username):
     summary = {
-        "Grupa społeczna": 'politycy',
+        "Grupa społeczna": "politycy",
         "Tweety o koronawirusie": 123,
         "Retweetowane": 23,
         "Własne tweety": 100,
         "Średnia ilość polubień": 67,
         "Średnia ilość retweetów": 23,
         "Najwięcej polubień": 249,
-        "Najwięcej retweetów": 35
+        "Najwięcej retweetów": 35,
     }
 
-    return render_template('user-summary.html', user=username, summary=summary)
+    return render_template("user-summary.html", user=username, summary=summary)
 
 
-@app.route('/user-tweets')
+@app.route("/user-tweets")
 def user_tweets_view():
     tweets = [
         {
-            'date_published': '2020-01-09',
-            'text': 'Głosuję na prezydenta Dudę prawdziwego prezydenta, nie lubię opozycji'
+            "date_published": "2020-01-09",
+            "text": "Głosuję na prezydenta Dudę prawdziwego prezydenta, nie lubię opozycji",
         },
         {
-            'date_published': '2020-01-10',
-            'text': 'A nie jednak nie lubię prezydenta Dudy  nie głosuję na niego'
+            "date_published": "2020-01-10",
+            "text": "A nie jednak nie lubię prezydenta Dudy  nie głosuję na niego",
         },
         {
-            'date_published': '2020-01-11',
-            'text': 'Nie wiem na kogo głosować mam gdzieś te wybory'
-        }
+            "date_published": "2020-01-11",
+            "text": "Nie wiem na kogo głosować mam gdzieś te wybory",
+        },
     ]
-    embed_tweet_html = get_embeddable_tweet_html_by_id(get_db()['Lekarze'].find({})[2]['id_str'])
-    return render_template('usertweets.html', tweets=tweets,
-                           embedded_tweet=embed_tweet_html)
+    embed_tweet_html = get_embeddable_tweet_html_by_id(
+        get_db()["Lekarze"].find({})[2]["id_str"]
+    )
+    return render_template(
+        "usertweets.html", tweets=tweets, embedded_tweet=embed_tweet_html
+    )
 
 
-@app.route('/user-tweets/<user_id>')
+@app.route("/user-tweets/<user_id>")
 def user_tweets_view_selected_user(user_id):
     pass
 
 
-@app.route('/user-tweets/<user_id>/<tweet_id>')
+@app.route("/user-tweets/<user_id>/<tweet_id>")
 def user_tweets_view_selected_tweet(user_id, tweet_id):
     pass
 
 
-@app.route('/user-groups')
+@app.route("/user-groups")
 def user_groups_view():
-    return render_template('user-groups.html', groups=get_tweet_source().get_user_groups())
+    return render_template(
+        "user-groups.html", groups=get_tweet_source().get_user_groups()
+    )
 
 
-@app.route('/tweet-test')
+@app.route("/tweet-test")
 def tweet_test_view():
-    t = get_db()['Lekarze'].find({})[0]['id_str']
+    t = get_db()["Lekarze"].find({})[0]["id_str"]
     html = get_embeddable_tweet_html_by_id(t)
-    return render_template('tweettest.html', xdd=html)
+    return render_template("tweettest.html", xdd=html)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/test-result-render")
+def test_result_rendering():
+    result = results.TextAnalysisResult("Example text analysis result")
+    result2 = results.DataFrameAnalysisResult(
+        pd.DataFrame({"xd": [2, 1], "yd": [3, 7]})
+    )
+
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([21, 37, 14, 88])
+    result3 = results.FigureAnalysisResult(fig)
+
+    composite_result = results.CompositeAnalysisResult(
+        text_analysis=result, dataframe_analysis=result2, figure_analysis=result3,
+    )
+    return render_template(
+        "test_result_render.html", result=composite_result.render_html()
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")

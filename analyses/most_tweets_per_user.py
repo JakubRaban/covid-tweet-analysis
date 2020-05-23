f8cbd8c4d62b2aa9ab49ab7af0c14be40f514089
Hubert Guzowski <hubert.guzowski@cyfronet.pl>
2020-05-23 11:29:06 +0200

    Installes Scikit-learn (needs entry in pipfile). Added per day analysis and expanded tweets per user analysis

--- analyses/most_tweets_per_user.py
+++ analyses/most_tweets_per_user.py
@@ -3,9 +3,15 @@ from analyses.results import DataFrameAnalysisResult
 from data_source import Tweets
 
 
-class SampleAnalysis(Analysis):
+class MostTweetsPerUser(Analysis):
+
     def run(self, tweets: Tweets) -> AnalysisResult:
         data = tweets.to_data_frame()
-        tweets_number = data.groupby('user_name').count()['text'].sort_values(ascending=False)
+        if self.params['top']:
+            tweets_number = data.groupby('user_name').count()['text'].\
+                sort_values(ascending=False).\
+                head(self.params['top'])
+        else:
+            tweets_number = data.groupby('user_name').count()['text'].sort_values(ascending=False)
 
         return DataFrameAnalysisResult(tweets_number)

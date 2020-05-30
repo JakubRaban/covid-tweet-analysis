a8c99f524f76bb350318f89c052e727f3a11a3ad
= <jakubgol2020@gmail.com>
2020-05-30 22:15:25 +0200

    modified_range_analyses

--- analyses/range_analysis.py
+++ analyses/range_analysis.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 class RangeAnalysis(Analysis):
     def __init__(self, weights=None, to_plot_amount=15):
         if not weights:
-            self.weights = {"tweets": 1, "retweets": 2, "replies": 1, "followers": 1 / 50}
+            self.weights = {"tweets": 1, "retweets": 2000, "replies": 1000, "followers": 1 / 50, "mentions": 1}
         else:
             self.weights = weights
         self.to_plot_amount = to_plot_amount
@@ -17,10 +17,36 @@ class RangeAnalysis(Analysis):
         data = tweets.to_data_frame()
 
         tweets_per_user = data.groupby('user_name').count()['text'].sort_values(ascending=False)
-
         followers_per_user = data[['user_name', 'user_followers_count']]
         followers_per_user = followers_per_user.groupby('user_name').max()
         followers_per_user = followers_per_user['user_followers_count']
+
+        mentions_per_user = data.groupby('user_name').count()
+        mentions_df_dict = {'user':[], 'mentions': []}
+        user_dict = {}
+        for user_name in mentions_per_user.index:
+            user_dict[user_name]=0
+
+        for index, row in data.iterrows():
+            if("NaN" in str(row['mentions_names'])):
+                continue
+            mentioned_names = str(row['mentions_names']).split(";")
+            for name in mentioned_names:
+                name = name.strip()
+                if(name!="nan"):
+                    if(name not in user_dict):
+                        continue
+                    user_dict[name] += row['user_followers_count']
+
+        for user, mentions in user_dict.items():
+            mentions_df_dict['user'].append(user)
+            mentions_df_dict['mentions'].append(user)
+        mentions_per_user_df = pd.DataFrame(mentions_df_dict)
+        mentions_per_user_df = mentions_per_user_df.groupby('user').sum()
+        mentions_per_user_df = mentions_per_user_df['mentions']
+
+
+
 
         grouped_by_name = data.groupby('user_name').sum()
 
@@ -31,7 +57,8 @@ class RangeAnalysis(Analysis):
         range_dict = {'tweets': tweets_per_user,
                       'retweets' : retweets_per_user,
                       'replies': reply_sum_per_user,
-                      'followers': followers_per_user}
+                      'followers': followers_per_user,
+                      'mentions': mentions_per_user_df}
 
         range_frame = pd.DataFrame(range_dict)
         range_frame['total_range'] = self.calculate_range(range_frame)
@@ -50,8 +77,8 @@ class RangeAnalysis(Analysis):
     def calculate_range(self, range_frame):
         total_range = range_frame["tweets"] * 0
         for key, value in self.weights.items():
-            if key != "tweets":
-                total_range += range_frame[key]
-            else:
+            if key == "tweets":
                 total_range += range_frame[key] * value * range_frame["followers"]
+            else:
+                total_range += range_frame[key]
         return total_range
